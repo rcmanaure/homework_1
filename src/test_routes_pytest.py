01b36e888d6bf9bd762e09c4b2a08b0030286704
rcmanaure <rcmanaure@gmail.com>
2022-10-17 18:33:43 -0400

    updated unit tests

--- src/test_routes_pytest.py
+++ src/test_routes_pytest.py
@@ -2,8 +2,8 @@ from services.app import app_test
 
 
 def test_home():
-    # Test the route to the main page.
-    response = app_test.test_client().get('/')
+    # Test the route to the swagger page.
+    response = app_test.test_client().get("/docs")
 
     assert response.status_code == 200
     assert response.status_code != 404
@@ -11,7 +11,7 @@ def test_home():
 
 def test_home_no_succes():
     # Test the route to a wrong url to main page.
-    response = app_test.test_client().get('/.')
+    response = app_test.test_client().get("/.")
 
     assert response.status_code == 404
     assert response.status_code != 200
@@ -19,7 +19,7 @@ def test_home_no_succes():
 
 def test_login():
     # Test the route to the login wrong page.
-    response = app_test.test_client().get('/loginn')
+    response = app_test.test_client().get("/loginn")
 
     assert response.status_code == 404
     assert response.status_code != 200
